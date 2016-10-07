9a121beca22795c1b014e22d4978b202d0d508cd
kingasawa <kingasawa@gmail.com>
2016-10-07 08:20:39 +0700

    upload icon , update check

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# Mua & Bán
+# Bitcoin Exchange
+_Using SailsJS Frameword_**by Khánh Trần**
+
+Contact Me
+---------
+
+* [Facebook](https://www.facebook.com/kingasawa.chan)
+* [Email](mailto:trancatkhanh@gmail.com)
--- assets/js/check.js
+++ assets/js/check.js
@@ -5,18 +5,18 @@ $(function() {
   $(document).ready(function() {
     $('#icon_done').hide();
     $('#icon_load').hide();
-    $('#taikhoan_khongdung').hide();
+    $('#icon_close').hide();
     var number = $('#number_vcb').val();
     $('#number_vcb').keyup(function () {
       $('#name_vcb').val("");
       $('#icon_done').hide();
       $('#icon_load').hide();
-      $('#taikhoan_khongdung').hide();
+      $('#icon_close').hide();
       if ($('#number_vcb').val().length == 13) {
         $('#icon_load').show();
         $('#icon_done').hide();
         $('#icon_put').hide();
-        $('#taikhoan_khongdung').hide();
+        $('#icon_close').hide();
         var a = getname_vcb(number);
       }
     });
@@ -34,14 +34,22 @@ $(function() {
         $('#icon_load').hide();
         $('#icon_done').show();
         if (data.msg=='N/A') {
-          $('#taikhoan_khongdung').show();
+          $('#icon_done').hide();
+          $('#icon_close').show();
+          $('input#name_vcb').css({'color':'#a94442'});
+          $('input#name_vcb').val('Tài khoản không tồn tại');
+          $('input#number_vcb').css({'border':'1px solid #a94442'});
+          $('input#number_vcb').css({'color':'#a94442'});
         } else {
           $('#name_vcb').val(data.msg);
           $('label.blue').css({'color':'#468847'});
-          $('input.blue').css({'border':'1px solid #468847'});
+          $('input#number_vcb').css({'border':'1px solid #3f8040'});
+          $('input#number_vcb').css({'color':'#3f8040'});
+          $('input#name_vcb').css({'color':'#3f8040'});
         }
       }
     });
+
     // $.ajax({
     //   url : url,
     //   type : "POST",
